7a686663991bd3fe7fc82ee39c000cf2f9014e9e
Mojtaba Zarezadeh <m.zarezadeh@inside.sahab.ir>
2022-08-14 15:00:52 +0430

    Refactor: Extract HdfsBase.

--- src/main/java/ir/sahab/hdfsrule/HdfsExtension.java
+++ src/main/java/ir/sahab/hdfsrule/HdfsExtension.java
@@ -1,88 +1,48 @@
 package ir.sahab.hdfsrule;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.DistributedFileSystem;
-import org.apache.hadoop.hdfs.MiniDFSCluster;
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.*;
 
 /**
  * A JUnit 5 extension for starting an HDFS server on the local machine.
  */
-public class HdfsExtension implements BeforeAllCallback, AfterAllCallback {
-    private MiniDFSCluster hdfsCluster = null;
-    private File miniClusterDataDir = null;
+public class HdfsExtension extends HdfsBase implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+
+    private final TestInstance.Lifecycle lifecycle;
+
+    public HdfsExtension() {
+        this(TestInstance.Lifecycle.PER_CLASS);
+    }
+
+    public HdfsExtension(TestInstance.Lifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
 
     @Override
     public void afterAll(ExtensionContext context) {
-        if (hdfsCluster != null && hdfsCluster.isClusterUp())
-            hdfsCluster.shutdown();
-        if (miniClusterDataDir != null) {
-            try {
-                FileUtils.deleteDirectory(miniClusterDataDir);
-            } catch (IOException e) {
-                throw new AssertionError("Failed to delete HDFS mini cluster's data directory.", e);
-            }
+        if (lifecycle == TestInstance.Lifecycle.PER_CLASS) {
+            teardown();
         }
     }
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        miniClusterDataDir = Files.createTempDirectory("hdfs-").toFile();
-        Configuration conf = new Configuration();
-        conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, miniClusterDataDir.getAbsolutePath());
-        hdfsCluster = new MiniDFSCluster.Builder(conf).build();
-        hdfsCluster.waitActive();
-    }
-
-    public URI getUri() {
-        return hdfsCluster.getURI();
-    }
-
-    public void writeString(Path path, String content) throws IOException {
-        write(path, content.getBytes());
-    }
-
-    public void write(Path path, byte[] content) throws IOException {
-        try (FileSystem fs = getFileSystem(); FSDataOutputStream outputStream = fs.create(path)) {
-            outputStream.write(content);
+        if (lifecycle == TestInstance.Lifecycle.PER_CLASS) {
+            setup();
         }
     }
 
-    public String readString(Path path) throws IOException {
-        return new String(read(path), StandardCharsets.UTF_8);
-    }
-
-    public byte[] read(Path path) throws IOException {
-        try (FileSystem fs = getFileSystem(); FSDataInputStream inputStream = fs.open(path)) {
-            return IOUtils.toByteArray(inputStream);
+    @Override
+    public void afterEach(ExtensionContext context) {
+        if (lifecycle == TestInstance.Lifecycle.PER_METHOD) {
+            teardown();
         }
     }
 
-    public String getNameNodeAddresses() {
-        StringBuilder addresses = new StringBuilder();
-        for (int i = 0; i < hdfsCluster.getNumNameNodes(); i++) {
-            addresses.append("127.0.0.1:").append(hdfsCluster.getNameNodeServicePort(i)).append(",");
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        if (lifecycle == TestInstance.Lifecycle.PER_METHOD) {
+            setup();
         }
-        // Remove the last comma.
-        addresses.deleteCharAt(addresses.length() - 1);
-        return addresses.toString();
-    }
-
-    public DistributedFileSystem getFileSystem() throws IOException {
-        return hdfsCluster.getFileSystem();
     }
 }
--- src/main/java/ir/sahab/hdfsrule/HdfsRule.java
+++ src/main/java/ir/sahab/hdfsrule/HdfsRule.java
@@ -1,86 +1,54 @@
 package ir.sahab.hdfsrule;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.junit.rules.ExternalResource;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hdfs.MiniDFSCluster;
+
+import java.io.IOException;
+import java.net.URI;
 
 /**
  * A JUnit 4 rule for starting an HDFS server on the local machine.
  */
 public class HdfsRule extends ExternalResource {
 
-    private MiniDFSCluster hdfsCluster = null;
-    private File miniClusterDataDir = null;
+    private final HdfsBase base = new HdfsBase();
 
     @Override
     protected void before() throws Throwable {
-        miniClusterDataDir = Files.createTempDirectory("hdfs-").toFile();
-        Configuration conf = new Configuration();
-        conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, miniClusterDataDir.getAbsolutePath());
-        hdfsCluster = new MiniDFSCluster.Builder(conf).build();
-        hdfsCluster.waitActive();
+        base.setup();
     }
 
     @Override
     protected void after() {
-        if (hdfsCluster != null && hdfsCluster.isClusterUp())
-            hdfsCluster.shutdown();
-        if (miniClusterDataDir != null) {
-            try {
-                FileUtils.deleteDirectory(miniClusterDataDir);
-            } catch (IOException e) {
-                throw new AssertionError("Failed to delete HDFS mini cluster's data directory.", e);
-            }
-        }
+        base.teardown();
     }
 
     public URI getUri() {
-        return hdfsCluster.getURI();
+        return base.getUri();
     }
 
     public void writeString(Path path, String content) throws IOException {
-        write(path, content.getBytes());
+        base.writeString(path, content);
     }
 
     public void write(Path path, byte[] content) throws IOException {
-        try (FileSystem fs = getFileSystem(); FSDataOutputStream outputStream = fs.create(path)) {
-            outputStream.write(content);
-        }
+        base.write(path, content);
     }
 
     public String readString(Path path) throws IOException {
-        return new String(read(path), StandardCharsets.UTF_8);
+        return base.readString(path);
     }
 
     public byte[] read(Path path) throws IOException {
-        try (FileSystem fs = getFileSystem(); FSDataInputStream inputStream = fs.open(path)) {
-            return IOUtils.toByteArray(inputStream);
-        }
+        return base.read(path);
     }
 
     public String getNameNodeAddresses() {
-        StringBuilder addresses = new StringBuilder();
-        for (int i = 0; i < hdfsCluster.getNumNameNodes(); i++) {
-            addresses.append("127.0.0.1:").append(hdfsCluster.getNameNodeServicePort(i)).append(",");
-        }
-        // Remove the last comma.
-        addresses.deleteCharAt(addresses.length() - 1);
-        return addresses.toString();
+        return base.getNameNodeAddresses();
     }
 
     public DistributedFileSystem getFileSystem() throws IOException {
-        return hdfsCluster.getFileSystem();
+        return base.getFileSystem();
     }
 }
